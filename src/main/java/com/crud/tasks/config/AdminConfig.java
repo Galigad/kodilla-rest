12c9ce14591ab18948eb883adc04c187411b8a95
Galigad <artur.szuberla@gmail.com>
2018-06-15 23:43:31 +0200

    Exercise 24.2

--- src/main/java/com/crud/tasks/config/AdminConfig.java
+++ src/main/java/com/crud/tasks/config/AdminConfig.java
@@ -9,4 +9,29 @@ import org.springframework.stereotype.Component;
 public class AdminConfig {
     @Value("${admin.mail}")
     private String adminMail;
+
+    @Value("${admin.name}")
+    private String adminName;
+
+    @Value("${info.app.name}")
+    private String appName;
+
+    @Value("${info.app.company.name}")
+    private String companyName;
+
+    @Value("${info.app.administrator.adress.stret}")
+    private String adminAdress;
+
+    @Value("${info.app.administrator.adress.number}")
+    private String adminAdressNumber;
+
+    @Value("${info.app.company.goal}")
+    private String companyGoal;
+
+    @Value("${info.app.company.email}")
+    private String companyEmail;
+
+    @Value("${info.app.company.phone}")
+    private String companyPhone;
+
 }
--- src/main/java/com/crud/tasks/service/MailCreatorService.java
+++ src/main/java/com/crud/tasks/service/MailCreatorService.java
@@ -1,5 +1,6 @@
 package com.crud.tasks.service;
 
+import com.crud.tasks.config.AdminConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
@@ -10,6 +11,9 @@ import org.thymeleaf.context.Context;
 public class MailCreatorService {
 
     @Autowired
+    private AdminConfig adminConfig;
+
+    @Autowired
     @Qualifier("templateEngine")
     private TemplateEngine templateEngine;
 
@@ -18,6 +22,14 @@ public class MailCreatorService {
         context.setVariable("message", message);
         context.setVariable("task_url", "http://galigad.github.io/crud");
         context.setVariable("button", "Visit website");
+        context.setVariable("admin_name", adminConfig.getAdminMail());
+        context.setVariable("app_name", adminConfig.getAppName());
+        context.setVariable("company_name", adminConfig.getCompanyName());
+        context.setVariable("company_adress", adminConfig.getAdminAdress());
+        context.setVariable("company_adress_num", adminConfig.getAdminAdressNumber());
+        context.setVariable("company_mail", adminConfig.getCompanyEmail());
+        context.setVariable("company_goal", adminConfig.getCompanyGoal());
+        context.setVariable("company_phone", adminConfig.getCompanyPhone());
         return templateEngine.process("mail/created-trello-card-mail", context);
     }
 }
